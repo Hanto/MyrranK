22a4b1d25d61339956b0fb437bb0a2197e508ff7
Ivan Delgado Huerta <jhanto@gmail.com>
2024-08-19 08:19:38 +0200

    [0177] work in progress

--- core/src/main/kotlin/com/myrran/domain/mobs/spells/spell/SpellConstants.kt
+++ core/src/main/kotlin/com/myrran/domain/mobs/spells/spell/SpellConstants.kt
@@ -1,6 +1,7 @@
 package com.myrran.domain.mobs.spells.spell
 
 import com.badlogic.gdx.physics.box2d.World
+import com.myrran.domain.skills.created.form.FormSkillSlotId
 import com.myrran.domain.skills.created.stat.StatId
 
 class SpellConstants {
@@ -11,6 +12,9 @@ class SpellConstants {
         val SPEED = StatId("SPEED")
         val SIZE = StatId("SIZE")
         val RANGE = StatId("RANGE")
+
+
+        val IMPACT_SLOT = FormSkillSlotId("IMPACT:1")
     }
 }
 
