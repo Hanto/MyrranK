cfe2ff548fed6a3958dad53d0ce47ca5e3a7d297
Ivan Delgado Huerta <jhanto@gmail.com>
2024-08-19 09:04:10 +0200

    [0177] work in progress

--- core/src/main/kotlin/com/myrran/domain/mobs/spells/form/Form.kt
+++ core/src/main/kotlin/com/myrran/domain/mobs/spells/form/Form.kt
@@ -1,4 +1,14 @@
 package com.myrran.domain.mobs.spells.form
 
-interface Form {
-}
+import com.badlogic.gdx.utils.Disposable
+import com.myrran.domain.misc.Identifiable
+import com.myrran.domain.mobs.common.Mob
+import com.myrran.domain.mobs.common.MobId
+import com.myrran.domain.mobs.common.colisionable.Collisioner
+import com.myrran.domain.mobs.common.consumable.Consumable
+import com.myrran.domain.mobs.common.steerable.Movable
+import com.myrran.domain.mobs.common.steerable.Spatial
+import com.myrran.domain.mobs.common.steerable.Steerable
+
+interface Form : Mob, Identifiable<MobId>, Steerable, Spatial, Movable, Disposable,
+    Consumable, Collisioner
